a2fa8996e5c1013d160ed376ab6cb8c932c2370a
Diogo F de Medeiros <diogo@diogofmedeiros.com>
2022-09-26 21:05:32 -0300

    Modulo 4, até Criando modal login (Parte 1)

---
+++ dashboard/src/components/ModalLogin/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
+    <button
+      @click="close"
+      class="text-4xl text-gray-600 focus:outline-none"
+    >
+      &times;
+    </button>
+  </div>
+
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          v-model="state.email.value"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="jane.dae@gmail.com"
+          type="email">
+      </label>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+import useModal from '@/hooks/useModal'
+
+export default {
+  name: 'loginModal',
+  setup () {
+    const modal = useModal()
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password: {
+        value: '',
+        errorMessage: ''
+      }
+    })
+
+    return {
+      state,
+      close: modal.close
+    }
+  }
+}
+</script>
---
+++ dashboard/src/views/Feedbacks/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="text-brand-main">Aqui é a Feedbacks</h1>
+</template>
+
+<script>
+export default {
+  name: 'feedbacksView'
+}
+</script>
